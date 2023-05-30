1703cf60f7d6971697484c411312f6e0b47f71b0
mkoseoglu091 <mkoseoglu091@gmail.com>
2023-05-30 15:06:32 -0700

    Health bar now receives health from player controller

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -107,6 +107,8 @@ private:
 	UFUNCTION()
 	void OnRep_Health();
 
+	class ABlasterPlayerController* BlasterPlayerController;
+
 public:
 	void SetOverlappingWeapon(AWeapon* Weapon);
 	bool IsWeaponEquipped();
